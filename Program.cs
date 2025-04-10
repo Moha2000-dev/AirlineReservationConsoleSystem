a429a4e42a878eac58e46ebeddd3cddc6696efb9
MOHAMMED <TOM@20377@GMAIL.COM>
2025-04-10 10:44:25 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Airline_Reservation_Console_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
